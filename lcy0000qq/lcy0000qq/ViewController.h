7d7ce556fd8df2587124d72663256747402693a0
chuangliu133130 <chuangliu133130@126.com>
2020-06-19 13:30:36 +0800

    ppp

---
+++ lcy0000qq/lcy0000qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lcy0000qq
+//
+//  Created by apple on 2020/6/19.
+//  Copyright © 2020 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lcy0000qq/lcy0000qqTests/lcy0000qqTests.m
@@ -0,0 +1,37 @@
+//
+//  lcy0000qqTests.m
+//  lcy0000qqTests
+//
+//  Created by apple on 2020/6/19.
+//  Copyright © 2020 liu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lcy0000qqTests : XCTestCase
+
+@end
+
+@implementation lcy0000qqTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
